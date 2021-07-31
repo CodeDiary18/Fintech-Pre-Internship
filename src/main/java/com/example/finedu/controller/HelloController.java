4962e9478e5709912f64d7798f1eea437ad48ac6
CodeDiary18 <codediary18@gmail.com>
2021-07-31 12:41:32 +0900

    Controller ResponseBody

---
+++ src/main/java/com/example/finedu/controller/HelloController.java
@@ -0,0 +1,42 @@
+package com.example.finedu.controller;
+
+import com.example.finedu.model.Hello;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello-mvc")
+    public String helloTemplate(String name, Model model){
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
+
+    @GetMapping("/hello-string")
+    @ResponseBody
+    public String helloString(String name){
+        return String.format("Hello, %s",name);
+    }
+
+    @GetMapping("/hello-map")
+    @ResponseBody
+    public Map<String,Object> helloMap(String name){
+        Map<String, Object> m = new HashMap<>();
+        m.put("name",name);
+        return m;
+    }
+
+    @GetMapping("/hello-object")
+    @ResponseBody
+    public Hello helloObject(String name){
+        Hello h = new Hello(name);
+        return h;
+    }
+}
---
+++ src/main/java/com/example/finedu/model/Hello.java
@@ -0,0 +1,9 @@
+package com.example.finedu.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+public class Hello {
+    final String name;
+}
