067b924d9adea8210251bdc40bfb06d66fbd2309
CodeDiary18 <codediary18@gmail.com>
2021-07-31 12:51:42 +0900

    RestController

--- src/main/java/com/example/finedu/controller/HelloController.java
+++ src/main/java/com/example/finedu/controller/HelloController.java
@@ -1,14 +1,8 @@
 package com.example.finedu.controller;
 
-import com.example.finedu.model.Hello;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 public class HelloController {
@@ -17,26 +11,5 @@ public class HelloController {
     public String helloTemplate(String name, Model model){
         model.addAttribute("name",name);
         return "hello-template";
-    }
-
-    @GetMapping("/hello-string")
-    @ResponseBody
-    public String helloString(String name){
-        return String.format("Hello, %s",name);
-    }
-
-    @GetMapping("/hello-map")
-    @ResponseBody
-    public Map<String,Object> helloMap(String name){
-        Map<String, Object> m = new HashMap<>();
-        m.put("name",name);
-        return m;
-    }
-
-    @GetMapping("/hello-object")
-    @ResponseBody
-    public Hello helloObject(String name){
-        Hello h = new Hello(name);
-        return h;
     }
 }
