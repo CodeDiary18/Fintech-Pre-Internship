dcbbe6f1d1eaad2ebd68257b3e8d97d6c5010cb1
CodeDiary18 <codediary18@gmail.com>
2021-07-31 10:45:29 +0900

    Initial Commit

---
+++ src/main/java/com/example/finedu/FineduApplication.java
@@ -0,0 +1,13 @@
+package com.example.finedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FineduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FineduApplication.class, args);
+	}
+
+}
