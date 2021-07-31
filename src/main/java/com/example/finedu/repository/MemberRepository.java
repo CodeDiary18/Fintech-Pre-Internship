dcadf2615548f2502e2f548383279b8d4f1052f9
CodeDiary18 <codediary18@gmail.com>
2021-07-31 16:54:36 +0900

    Data Rest base path 설정

--- src/main/java/com/example/finedu/repository/MemberRepository.java
+++ src/main/java/com/example/finedu/repository/MemberRepository.java
@@ -3,5 +3,7 @@ package com.example.finedu.repository;
 import com.example.finedu.entity.Member;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface MemberRepository extends PagingAndSortingRepository<Member, Long> {    //members라는 api 자동 생성
+public interface MemberRepository extends PagingAndSortingRepository<Member, Long> {
+    // build.gradle에 "implementation "org.springframework.boot:spring-boot-starter-data-rest" 이것 때문에
+    // members라는 api 자동 생성
 }
