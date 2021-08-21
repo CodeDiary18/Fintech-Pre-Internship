7e0e8b2865788501199f02b7661a9c33970dbffb
roehd <kieun0125@gmail.com>
2021-08-22 00:13:38 +0900

    상환금액 오류처리(금액초과,모인금액없음)

--- src/main/java/com/example/hackathon/controller/MyPageApi.java
+++ src/main/java/com/example/hackathon/controller/MyPageApi.java
@@ -17,7 +17,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/mypage")
@@ -29,13 +33,17 @@ public class MyPageApi {
 
     @GetMapping("/loan")
     public String loanList(@AuthenticationPrincipal UserInfo userInfo, Model model) {
-        List<LoanModel> loans = loanService.findUserLoans(userInfo.getSeq());
+        List<LoanModel> loans = loanService
+                .findUserLoans(userInfo.getSeq())
+                .stream()
+                .filter(loan -> loan.getPermit() == 1)
+                .collect(Collectors.toList());
         model.addAttribute("loans", loans);
         return "mypage/loanList";
     }
 
     @GetMapping("/invest")
-    public String investList(@AuthenticationPrincipal UserInfo userInfo, Model model) {
+    public String investList(@AuthenticationPrincipal UserInfo userInfo, Model model){
         List<InvestModel> invests = investService.findAllInvested(userInfo.getSeq());
         List<ApprovedLoanModel> approvedLoanModels = approvedLoanService.findMyPageApproved(invests);
         List<LoanModel> loanModels = loanService.findMyPageLoan(approvedLoanModels);
@@ -48,10 +56,27 @@ public class MyPageApi {
 
 
     @PostMapping("/repay/{id}")
-    public String repay(@PathVariable("id") Long loan_id, ApprovedLoanDto approvedLoanDto) {
+    public String repay(@PathVariable("id") Long loan_id, ApprovedLoanDto approvedLoanDto,
+                        HttpServletResponse response) throws IOException{
 
-        approvedLoanService.updateRepayment(loan_id, approvedLoanDto.getRepayment());
+        response.setContentType("text/html; charset=UTF-8");
+        PrintWriter out = response.getWriter();
 
-        return "redirect:/mypage/loan";
+        // 1:모인금액 범위 안에서 상환 완료, 0:상환할 금액이 없음, -1:범위 초과해서 상환함
+        int result = approvedLoanService.updateRepayment(loan_id, approvedLoanDto);
+        switch(result){
+            case 1:
+                out.println("<script>alert('상환이 성공적으로 처리되었습니다.'); location.href='/mypage'</script>");
+                break;
+            case 0:
+                out.println("<script>alert('상환할 금액이 없습니다.'); history.go(-1);</script>");
+                break;
+            case -1:
+                out.println("<script>alert('모인 금액을 초과한 값입니다.'); history.go(-1);</script>");
+                break;
+        }
+        out.flush();
+        return "";
+        //return "redirect:/mypage/loan";
     }
 }
